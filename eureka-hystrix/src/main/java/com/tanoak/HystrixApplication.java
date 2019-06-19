8908a11ba5038c7ec8448e4b5beff7c15883a3f6
tanoak <tanoak10@163.com>
2019-06-20 07:38:46 +0800

    增加hystrix

---
+++ eureka-hystrix/src/main/java/com/tanoak/HystrixApplication.java
@@ -0,0 +1,23 @@
+package com.tanoak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * @author tanoak@qq.com
+ * @version 1.0
+ * @date 2019/4/30 20:36
+ * @Description 启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class HystrixApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+    
+}
---
+++ eureka-hystrix/src/main/java/com/tanoak/web/ConsumerController.java
@@ -0,0 +1,22 @@
+package com.tanoak.web;
+
+import com.tanoak.service.ComputeClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class ConsumerController {
+
+    @Resource
+    ComputeClient computeClient;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add() {
+        return computeClient.add(10, 20);
+    }
+
+}
